59ce8a98767601b06e0f64c3dd0d0e8230128471
mk <skaranizadeh@csus.edu>
2023-12-15 10:18:33 -0800

    first commit MasterChallenge

---
+++ Packages/src/dev/moj/Main.java
@@ -0,0 +1,10 @@
+package dev.moj;
+
+import com.abc.first.Item;
+
+public class Main {
+    public static void main(String[] args) {
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
